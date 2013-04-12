dd3ea84b1f4b546ef51ec9dc176c6322ad69983b
hujiazhen <hujiazhen@gmail.com>
2013-04-12 14:28:07 +0800

    add refresh function

--- gen/com/zjuhjz/yacleaner/R.java
+++ gen/com/zjuhjz/yacleaner/R.java
@@ -36,6 +36,7 @@ public final class R {
         public static final int cleanbtn=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int nodata=0x7f040005;
         public static final int title_activity_process_list=0x7f040003;
     }
     public static final class style {
--- src/com/zjuhjz/yacleaner/ProcessList.java
+++ src/com/zjuhjz/yacleaner/ProcessList.java
@@ -2,6 +2,7 @@ package com.zjuhjz.yacleaner;
 
 //import com.example.android.supportv4.R;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -23,69 +24,92 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+
+//import com.example.android.supportv4.app.LoaderThrottleSupport.MainTable;
+
+import android.content.ContentResolver;
+//import android.content.ContentValues;
 import android.content.Context;
 
 public class ProcessList extends ListFragment {
 
 	private static List<RunningAppProcessInfo> procList = null;
 	static final int POPULATE_ID = Menu.FIRST;
-    static final int CLEAR_ID = Menu.FIRST+1;
-    
+	static final int CLEAR_ID = Menu.FIRST + 1;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_process_list);
+		// setContentView(R.layout.activity_process_list);
 	}
+
 	@Override
-	public void  onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        setHasOptionsMenu(true);
-        getProcessInfo();
-        showProcessInfo();
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setHasOptionsMenu(true);
+		getProcessInfo();
+		showProcessInfo();
 	}
-	@Override 
+
+	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        MenuItem populateItem = menu.add(Menu.NONE, POPULATE_ID, 0, "Populate");
-        populateItem.setIcon(R.drawable.ic_menu_refresh);
-        MenuItemCompat.setShowAsAction(populateItem, MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
-        MenuItem clearItem = menu.add(Menu.NONE, CLEAR_ID, 0, "Clear");
-        MenuItemCompat.setShowAsAction(clearItem, MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
-    }
+		MenuItem populateItem = menu.add(Menu.NONE, POPULATE_ID, 0, "Populate");
+		populateItem.setIcon(R.drawable.ic_menu_refresh);
+		MenuItemCompat.setShowAsAction(populateItem,
+				MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
+	}
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-	{
-		//super.onCreateView(inflater, container, savedInstanceState);
-		View view=inflater.inflate(R.layout.activity_process_list, container,false);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.activity_process_list, container,
+				false);
 		return view;
 	}
-	public void showProcessInfo() {
-		Context ctext= getActivity();
-        // 更新进程列表
-        List<HashMap<String,String>> infoList = new ArrayList<HashMap<String,String>>();
-        for (Iterator<RunningAppProcessInfo> iterator = procList.iterator(); iterator.hasNext();) {
-            RunningAppProcessInfo procInfo = iterator.next();
-            HashMap<String, String> map = new HashMap<String, String>();
-            map.put("proc_name", procInfo.processName);
-            map.put("proc_id", procInfo.pid+"");
-            infoList.add(map);
-        }
-        
-        SimpleAdapter simpleAdapter = new SimpleAdapter(
-        		ctext,
-                infoList, 
-                R.layout.process_list_item, 
-                new String[]{"proc_name"},
-                new int[]{R.id.process_name} );
-        setListAdapter(simpleAdapter);
-        TextView textview = (TextView)getView().findViewById(R.id.total_process_num);
-        textview.setText("Total Process Num:"+Integer.toString(infoList.size()));
-    }
-	public int getProcessInfo() {
-		Context ctext= getActivity();
-        ActivityManager activityManager = (ActivityManager) ctext.getSystemService(Context.ACTIVITY_SERVICE);
-        procList = activityManager.getRunningAppProcesses();
-        return procList.size();
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case POPULATE_ID:
+			getProcessInfo();
+			showProcessInfo();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
-	
+
+	public void showProcessInfo() {
+		Context ctext = getActivity();
+		// 更新进程列表
+		List<HashMap<String, String>> infoList = new ArrayList<HashMap<String, String>>();
+		for (Iterator<RunningAppProcessInfo> iterator = procList.iterator(); iterator
+				.hasNext();) {
+			RunningAppProcessInfo procInfo = iterator.next();
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put("proc_name", procInfo.processName);
+			map.put("proc_id", procInfo.pid + "");
+			infoList.add(map);
+		}
+
+		SimpleAdapter simpleAdapter = new SimpleAdapter(ctext, infoList,
+				R.layout.process_list_item, new String[] { "proc_name" },
+				new int[] { R.id.process_name });
+		setListAdapter(simpleAdapter);
+		TextView textview = (TextView) getView().findViewById(
+				R.id.total_process_num);
+		textview.setText("Total Process Num: "
+				+ Integer.toString(infoList.size()));
+	}
+
+	public int getProcessInfo() {
+		Context ctext = getActivity();
+		ActivityManager activityManager = (ActivityManager) ctext
+				.getSystemService(Context.ACTIVITY_SERVICE);
+		procList = activityManager.getRunningAppProcesses();
+		return procList.size();
+	}
 
 }
