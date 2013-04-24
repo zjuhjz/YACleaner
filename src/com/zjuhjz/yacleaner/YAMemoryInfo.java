689f6024707c7f52c0a82e1f441a901d850f5b3b
hujiazhen <hujiazhen@gmail.com>
2013-04-24 20:17:12 +0800

    fix

--- src/com/zjuhjz/yacleaner/YAMemoryInfo.java
+++ src/com/zjuhjz/yacleaner/YAMemoryInfo.java
@@ -20,85 +20,81 @@ import com.zjuhjz.yacleaner.tool.*;
 public class YAMemoryInfo {
 	public long totalMemory;
 	public long totalUsed;
-	
+
 	private static List<RunningAppProcessInfo> runningAppProcesses = null;
 	private static MemoryInfo mi = new MemoryInfo();
-	//static final int POPULATE_ID = Menu.FIRST;
-	//static final int CLEAR_ID = Menu.FIRST + 1;
+	// static final int POPULATE_ID = Menu.FIRST;
+	// static final int CLEAR_ID = Menu.FIRST + 1;
 	Context context;
 	List<HashMap<String, String>> processInfoList;
 	List<HashMap<String, String>> servicesInfoList;
-	CMDExecute cmdExecute= new CMDExecute();
-	
-	YAMemoryInfo(Context context){
+	CMDExecute cmdExecute = new CMDExecute();
+
+	YAMemoryInfo(Context context) {
 		processInfoList = new ArrayList<HashMap<String, String>>();
 		servicesInfoList = new ArrayList<HashMap<String, String>>();
 		this.context = context;
 		refresh();
 	}
-	
-	public boolean refresh(){
+
+	public boolean refresh() {
 		refreshProcessInfo();
-		
+
 		return true;
 	}
-	
+
 	public int refreshProcessInfo() {
-		//ApplicationInfo ai
+		// ApplicationInfo ai
 		ApplicationInfo ai;
-		
-		//PackageManager pm
+
+		// PackageManager pm
 		PackageManager pm = context.getPackageManager();
-		
-		//memory usage
-		HashMap<String,String> memoryUsage = new HashMap<String,String>();
-		
-		/////////initialize///////////
-		//ActivityManager activityManager
+
+		// memory usage
+		HashMap<String, String> memoryUsage = new HashMap<String, String>();
+
+		// ///////initialize///////////
+		// ActivityManager activityManager
 		ActivityManager activityManager = (ActivityManager) context
 				.getSystemService(Context.ACTIVITY_SERVICE);
-		//clear
-		if(runningAppProcesses!=null){
+		// clear
+		if (runningAppProcesses != null) {
 			runningAppProcesses.clear();
 		}
-		if(processInfoList!=null){
+		if (processInfoList != null) {
 			processInfoList.clear();
 		}
-			
+
 		runningAppProcesses = activityManager.getRunningAppProcesses();
-		///////initialize end/////////
+		// /////initialize end/////////
 		String result = null;
-		String[] items = null; 
+		String[] items = null;
+		String[] itempieces;
 		try {
-			//String[] args = { "/system/bin/ps", "-n","1" };
-			String[] args = { "/system/bin/ps"};
+			String[] args = { "/system/bin/ps" };
 			result = cmdExecute.run(args, ".");
-			//Log.d("yacleanerdebug", "result=" + result);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-		if(result!=null&&!result.isEmpty()){
+		if (result != null && !result.isEmpty()) {
 			items = result.split("\n");
 		}
-		int i = 0;
-		String [] item2;
-		for(String item:items){
-			i++;
-			//Log.d("yacleanerdebug",Integer.toString(i)+item);
-			item2=item.trim().split("\\s+");
-			if(item2.length>8){
-				//Log.d("yacleanerdebug",item2[8]+" : "+item2[4]);
-				memoryUsage.put(item2[8],  Integer.toString(Integer.parseInt(item2[4])/1024));
+
+		for (String item : items) {
+			itempieces = item.trim().split("\\s+");
+			if (itempieces.length > 8) {
+				memoryUsage.put(itempieces[8],
+						Integer.toString(Integer.parseInt(itempieces[4]) / 1024));
 			}
-			
+
 		}
-		
-		//get Memory info
-		//activityManager.getMemoryInfo(mi);
-		
-		//add info to processInfoList
-		for (Iterator<RunningAppProcessInfo> iterator = runningAppProcesses.iterator(); iterator
-				.hasNext();) {
+
+		// get Memory info
+		// activityManager.getMemoryInfo(mi);
+
+		// add info to processInfoList
+		for (Iterator<RunningAppProcessInfo> iterator = runningAppProcesses
+				.iterator(); iterator.hasNext();) {
 			RunningAppProcessInfo procInfo = iterator.next();
 			HashMap<String, String> map = new HashMap<String, String>();
 			try {
@@ -110,12 +106,12 @@ public class YAMemoryInfo {
 					.getApplicationLabel(ai) : procInfo.processName);
 			map.put("app_name", applicationName);
 			map.put("package_name", procInfo.processName);
-			map.put("pid", procInfo.pid+"");
-			map.put("memory_usage", memoryUsage.get(procInfo.processName)+"MB");
+			map.put("pid", procInfo.pid + "");
+			map.put("memory_usage", memoryUsage.get(procInfo.processName)
+					+ "MB");
 			processInfoList.add(map);
 		}
 		return runningAppProcesses.size();
 	}
-	
-	
+
 }
